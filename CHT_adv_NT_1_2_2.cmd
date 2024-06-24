0336c6a3b38cb6dd90d4c2a38e57424c6451006a
Joey Hsiao <Joey.Hsiao@quantatw.com>
2024-06-24 15:35:07 +0800

    Support CHT-adv NT 1-2-2 test script

---
+++ CHT_adv_NT_1_2_2.cmd
@@ -0,0 +1,32 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "root_folder=%~dp0"
+
+copy %root_folder%\configs\config_NT_1-2-2.ini %root_folder%\_config.ini
+
+call "%root_folder%\cmds\cmd_get_config_value.cmd"
+call "%root_folder%\cmds\cmd_wait_adb_all.cmd"
+
+: clean UE logs and tmp files 
+del /Q %root_folder%\tmp\*
+for /f "skip=1 tokens=1" %%a in ('%root_folder%\adb_tool\adb devices ^| findstr /r /b /c:"[0-9A-Za-z]"') do (
+    %root_folder%\adb_tool\adb -s %%a shell "rm *xq3* /data/iperf3* /data/ping* /data/http* /data/ftp* /data/xq3* > /dev/null 2>&1"
+)
+
+echo .
+echo http
+call "%root_folder%\cmds\cmd_run_devices_for_http.cmd"
+
+REM Wait for 3600 seconds
+timeout /t 3600 /nobreak
+call "%root_folder%\testing_stop.cmd"
+
+echo .
+echo ftp
+call "%root_folder%\cmds\cmd_run_devices_for_ftp.cmd"
+
+echo CHT adv NT 1-3-1 finish !!!
+
+endlocal
+pause
