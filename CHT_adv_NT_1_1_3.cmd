2af1b63f7483df8b6dbd2c94472a0e1ecc54ec28
Joey Hsiao <Joey.Hsiao@quantatw.com>
2024-06-24 21:33:13 +0800

    Remove all logs before new test

--- CHT_adv_NT_1_1_3.cmd
+++ CHT_adv_NT_1_1_3.cmd
@@ -6,7 +6,7 @@ set "root_folder=%~dp0"
 copy %root_folder%\configs\config_NT_1-1-3.ini %root_folder%\_config.ini
 
 set "OTHERS_WAIT_CMD_FINISH=True"
-%root_folder%\adb_tool\adb shell "rm *xq3* /data/iperf3* /data/ping* /data/http* /data/ftp* /data/xq3* > /dev/null 2>&1"
+%root_folder%\adb_tool\adb shell "rm *xq3* /data/iperf3* /data/ping* /data/http* /data/ftp* /data/xq3* /data/xq3/* > /dev/null 2>&1"
 
 call "%root_folder%\cmds\cmd_get_config_value.cmd"
 call "%root_folder%\cmds\cmd_wait_adb_all.cmd"
