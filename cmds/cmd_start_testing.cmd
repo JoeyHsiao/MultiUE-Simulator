da4a5b623e1e7a46c50ce8f6d09b75196dc515aa
Joey Hsiao <Joey.Hsiao@quantatw.com>
2024-06-24 21:11:05 +0800

    Re-link library lftp and kill iperf3 lftp axel process before testing

--- cmds/cmd_start_testing.cmd
+++ cmds/cmd_start_testing.cmd
@@ -5,6 +5,12 @@ set "device_serial_name=%~1"
 
 set "root_folder=%~dp0"
 
+%root_folder%\..\adb_tool\adb -s %device_serial_name% push %root_folder%\..\scripts\libreadline.so.8.1 /usr/lib/
+%root_folder%\..\adb_tool\adb -s %device_serial_name% shell chmod +x /usr/lib/libreadline.so.8.1
+%root_folder%\..\adb_tool\adb -s %device_serial_name% shell ln -s /usr/lib/libreadline.so.8.1  /usr/lib/libreadline.so.8
+
+%root_folder%\..\adb_tool\adb -s %device_serial_name% shell killall iperf3 lftp axel
+
 %root_folder%\..\adb_tool\adb -s %device_serial_name% push %root_folder%\..\tmp/xq3_execute_config_%device_serial_name% /xq3_execute_config
 %root_folder%\..\adb_tool\adb -s %device_serial_name% push %root_folder%\..\tmp/xq3_execute_%device_serial_name% xq3_execute.sh
 %root_folder%\..\adb_tool\adb -s %device_serial_name% push %root_folder%\..\scripts/xq3_remove_CRLF.sh /
