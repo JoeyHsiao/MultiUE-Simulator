e71c1269af166e3be2c84e51e45875df8712e869
Joey Hsiao <Joey.Hsiao@quantatw.com>
2024-06-25 19:43:49 +0800

    When 60 min have passed, review the previous test content and proceed to the next test.

--- CHT_adv_NT_1_2_2.cmd
+++ CHT_adv_NT_1_2_2.cmd
@@ -21,11 +21,31 @@ call "%root_folder%\cmds\cmd_run_devices_for_http.cmd"
 
 REM Wait for 3600 seconds
 timeout /t 3600 /nobreak
-call "%root_folder%\testing_stop.cmd"
+
+echo .
+echo stop and change to ftp testing 
+for /f "skip=1 tokens=1" %%a in ('%root_folder%\adb_tool\adb devices ^| findstr /r /b /c:"[0-9A-Za-z]"') do (
+    echo %%a
+
+    %root_folder%\adb_tool\adb -s %%a shell /etc/init.d/xq3_testing stop
+    %root_folder%\adb_tool\adb -s %%a shell "killall iperf3 lftp axel  > /dev/null 2>&1"
+)
 
 echo .
 echo ftp
 call "%root_folder%\cmds\cmd_run_devices_for_ftp.cmd"
+
+REM Wait for 3600 seconds
+timeout /t 3600 /nobreak
+
+echo .
+echo stop and change to ftp testing 
+for /f "skip=1 tokens=1" %%a in ('%root_folder%\adb_tool\adb devices ^| findstr /r /b /c:"[0-9A-Za-z]"') do (
+    echo %%a
+
+    %root_folder%\adb_tool\adb -s %%a shell /etc/init.d/xq3_testing stop
+    %root_folder%\adb_tool\adb -s %%a shell "killall iperf3 lftp axel  > /dev/null 2>&1"
+)
 
 echo CHT adv NT 1-3-1 finish !!!
 
